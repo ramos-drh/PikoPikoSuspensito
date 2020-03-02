3adfdf24f28ffaa4005dccaa423b0cd86ea2a536
Daniel Rasero Marín <danielraseromarin@gmail.com>
2020-03-02 13:32:08 +0100

    -Creación de clase jugador con sus atributos y constructor

---
+++ src/main/java/com/mycompany/gusanito/Jugador.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.gusanito;
+
+/**
+ *
+ * @author daniel
+ */
+public class Jugador {
+    
+    private String nombre;
+    private int puntosAcum;
+    private Dado[] dados;
+
+    public Jugador(String nombre) {
+        this.nombre = nombre;
+        
+    }
+    
+    
+    
+}
