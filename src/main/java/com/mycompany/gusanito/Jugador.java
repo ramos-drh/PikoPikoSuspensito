30779505c613095c80ad1f0eb0e1a6ae6fd97b4e
Daniel Rasero Marín <danielraseromarin@gmail.com>
2020-03-02 13:33:28 +0100

    -Getters, setters y toString de clase jugador

--- src/main/java/com/mycompany/gusanito/Jugador.java
+++ src/main/java/com/mycompany/gusanito/Jugador.java
@@ -19,6 +19,35 @@ public class Jugador {
         this.nombre = nombre;
         
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPuntosAcum() {
+        return puntosAcum;
+    }
+
+    public void setPuntosAcum(int puntosAcum) {
+        this.puntosAcum = puntosAcum;
+    }
+
+    public Dado[] getDados() {
+        return dados;
+    }
+
+    public void setDados(Dado[] dados) {
+        this.dados = dados;
+    }
+
+    @Override
+    public String toString() {
+        return "Jugador{" + "nombre=" + nombre + ", puntosAcum=" + puntosAcum + ", dados=" + dados + '}';
+    }
     
     
     
